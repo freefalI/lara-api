cb86bfc7fa4addeab61a72d9ba17900567a167e6
freefall <andriyshmatko1999@gmail.com>
2019-06-13 23:59:57 +0300

    add migration for Tasks tabe

---
+++ database/migrations/2019_06_13_205329_create_tasks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->unsignedInteger('owner');
+            $table->boolean('status');
+            $table->timestamp('closed_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
